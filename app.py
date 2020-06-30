3ad191063cb012fa203074369e7c2838c96411a0
Daniele Marcocci <daniele.marcocci@outlook.com>
2020-06-30 09:52:44 +0200

    latest fixes

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import datetime
 import sys
 import os
@@ -15,12 +14,15 @@ REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
 
 DEBUG = os.environ.get("DEBUG", False)
 
+
 def timestamper(_, __, event_dict):
     event_dict["time"] = datetime.datetime.now().isoformat()
     return event_dict
 
 
-logging.basicConfig(format="%(message)s", level=logging.DEBUG if DEBUG else logging.INFO)
+logging.basicConfig(
+    format="%(message)s", level=logging.DEBUG if DEBUG else logging.INFO
+)
 
 structlog.configure(
     processors=[
@@ -34,40 +36,49 @@ structlog.configure(
     logger_factory=structlog.stdlib.LoggerFactory(),
     wrapper_class=structlog.stdlib.BoundLogger,
     cache_logger_on_first_use=True,
-    context_class=structlog.threadlocal.wrap_dict(dict)
+    context_class=structlog.threadlocal.wrap_dict(dict),
 )
 
 logger = structlog.get_logger()
 
 # disable log message from flask
-logging.getLogger('werkzeug').setLevel(logging.ERROR)
-os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
+os.environ["WERKZEUG_RUN_MAIN"] = "true"
+
 
 def get_redis_connection():
     if not REDIS_PASSWORD:
         return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DATABASE)
-    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DATABASE, password=REDIS_PASSWORD)
+    return redis.Redis(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DATABASE, password=REDIS_PASSWORD
+    )
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     logger.info("http_request", context="/")
-    return jsonify({
-        "context" : "/", 
-        "method" : "index"
-    })
+    return jsonify({"context": "/", "method": "index"})
+
 
 @app.route("/random")
 def _random():
     logger.info("http_request", context="/random")
-    return jsonify({"number" : random.randint(0,100000)})
+    return jsonify({"number": random.randint(0, 100000)})
+
+
+@app.route("/hello")
+def hello():
+    return jsonify({"context": "/hello", "time": datetime.datetime.now().isoformat()})
 
 
 @app.route("/health/liveness")
 def liveness():
     logger.debug("liveness", status="ok")
-    return jsonify({"status" : "ok"}), 200
+    return jsonify({"status": "ok"}), 200
+
 
 @app.route("/health/readiness")
 def readiness():
@@ -76,18 +87,12 @@ def readiness():
         r.ping()
         redis_alive = True
         logger.debug("readiness", status="ok")
-        return jsonify({
-            "status": "ok", 
-            "redis_alive":  redis_alive
-        }), 200
-        
+        return jsonify({"status": "ok", "redis_alive": redis_alive}), 200
+
     except:
         redis_alive = False
     logger.error("readiness", status="ko")
-    return jsonify({
-        "status": "ok", 
-        "redis_alive":  redis_alive
-    }), 500
+    return jsonify({"status": "ok", "redis_alive": redis_alive}), 500
 
 
 if __name__ == "__main__":
